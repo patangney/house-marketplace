edc20a4e5b9484896fdc4e24e48121df701d59ff
Patrick Tangney <ptangney@outlook.com>
2022-06-12 20:36:49 +0100

    setup navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
-import Profile from './pages/Profile';
+// import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
+      <Navbar />
     </Router>
     
     </>
