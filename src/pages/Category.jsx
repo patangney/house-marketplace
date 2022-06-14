86675edece87dd13b0c8440111f670624d3e11af
Patrick Tangney <ptangney@outlook.com>
2022-06-14 11:59:10 +0100

    setup explore page

---
+++ src/pages/Category.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+
+function Category () {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        // Get reference
+        const listingRef = collection(db, 'listings')
+        //Create a query
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchListing()
+  }, [])
+  return <div>Category</div>
+}
+
+export default Category
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
-function Explore() {
+function Explore () {
   return (
-    <div>
-        <h1>Explore</h1>
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
+      <main>
+        {/** @TODO slider to go here */}
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              className='exploreCategoryImg'
+              alt='rent'
+            />
+            <p className='exploreCategoryName'>Places for rent</p>
+          </Link>
+
+          <Link to='/category/sell'>
+            <img
+              src={sellCategoryImage}
+              className='exploreCategoryImg'
+              alt='sell'
+            />
+            <p className='exploreCategoryName'>Places for sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
