3b23a360117b23b4c9b41a8cc43b327179703d36
Patrick Tangney <ptangney@outlook.com>
2022-06-16 10:17:13 +0100

    added Listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,14 +1,12 @@
-import {useState, useEffect} from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { getDoc, doc, DocumentSnapshot } from 'firebase/firestore';
-import {db} from '../firebase.config'
-import { getAuth } from 'firebase/auth';
-import Spinner from './../components/Spinner';
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { getDoc, doc, DocumentSnapshot } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { getAuth } from 'firebase/auth'
+import Spinner from './../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
 
-
-
-function Listing() {
+function Listing () {
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
   const [shareLinkCopied, setShareLinkCopied] = useState(false)
@@ -22,19 +20,74 @@ function Listing() {
       const docRef = doc(db, 'listings', params.listingId) //getting id from url
       const docSnapshot = await getDoc(docRef)
 
-      if(docSnapshot.exists()){
+      if (docSnapshot.exists()) {
         console.log(docSnapshot)
         setListing(docSnapshot.data())
         setLoading(false)
       }
     }
     fetchListing()
-
   }, [navigate, params.listingId])
 
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
-    /** @TODO SLIDER */
-    
+    <main>
+      {/** @TODO SLIDER */}
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            //set shared link
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
+        <img src={shareIcon} alt='' />
+      </div>
+      {shareLinkCopied && <p className='linkCopied'>Link Copied</p>}
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - €
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            €{listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+        <ul className='listingDetailsList'>
+          <li>{listing.bedrooms > 1 ? `${listing.bedrooms}` : '1 Bedroom'}</li>
+          <li>
+            {listing.bathrooms > 1 ? `${listing.bathrooms}` : '1 Bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+        <p className='listingLocationTitle'>{/** @TODO  Map to go here */}</p>
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className='primaryButton'
+          >Contact Landlord</Link> //use this params to auto fill email
+        )}
+      </div>
+    </main>
   )
 }
 
